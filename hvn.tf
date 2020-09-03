17b141349e55a8cd85b9be61c43b7267789a4aed
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-09-03 12:35:29 -0400

    Revert to count

--- hvn.tf
+++ hvn.tf
@@ -11,8 +11,8 @@ resource "aws_vpc_peering_connection_accepter" "hvn" {
 }
 
 resource "aws_route" "hvn" {
-  for_each                  = toset(var.route_table_ids)
-  route_table_id            = each.value
+  count                     = length(var.route_table_ids)
+  route_table_id            = var.route_table_ids[count.index]
   destination_cidr_block    = var.hvn_cidr_block
   vpc_peering_connection_id = data.aws_vpc_peering_connection.hvn.id
 }
