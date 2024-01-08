316c03e5c1d855b98a00ae44c8c1acd2bfdc0bcb
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-01-08 14:53:57 -0500

    Output Role ARN

--- modules/boundary-worker/outputs.tf
+++ modules/boundary-worker/outputs.tf
@@ -7,3 +7,8 @@ output "security_group_id" {
   value       = var.worker_security_group_id != null ? var.worker_security_group_id : aws_security_group.worker.0.id
   description = "Security group for worker"
 }
+
+output "role_arn" {
+  value       = aws_iam_role.boundary.arn
+  description = "Role ARN for Boundary worker"
+}
