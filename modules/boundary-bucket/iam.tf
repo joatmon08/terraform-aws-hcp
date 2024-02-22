0cce3345cd0ab5409c84c33b620c307a0586c45d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-02-22 10:55:01 -0500

    Fix IAM policy

--- modules/boundary-bucket/iam.tf
+++ modules/boundary-bucket/iam.tf
@@ -2,9 +2,17 @@ data "aws_iam_policy_document" "boundary" {
   statement {
     effect = "Allow"
     actions = [
+      "s3:ListBucket",
+    ]
+    resources = ["${aws_s3_bucket.boundary.arn}"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
       "s3:PutObject",
       "s3:GetObject",
-      "s3:GetObjectAttributes"
+      "s3:GetObjectAttributes",
     ]
     resources = ["${aws_s3_bucket.boundary.arn}/*"]
   }
